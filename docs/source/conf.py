d7d5e2b1b28951c35455b748aab37941f2278406
DrJStrudwick <jstrudwick@hotmail.com>
2021-03-04 19:32:21 +0000

    doc syn

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../package/synthSportPlayer'))
+sys.path.insert(0, os.path.abspath('../../package/synthSportPlayer'))
 
 
 # -- Project information -----------------------------------------------------
