ab1e4890da453acea46a490c0d9efa0fd9641840
DrJStrudwick <jstrudwick@hotmail.com>
2021-03-04 18:08:44 +0000

    doc sync

--- docs/build/html/_sources/installation.rst.txt
+++ docs/build/html/_sources/installation.rst.txt
@@ -1,2 +1,10 @@
 Installation
 ============
+Install the package with:
+```
+pip install synthSportPlayer-DJS
+```
+import with:
+```
+import synthSportPlayer
+```
--- package/synthSportPlayer/sportplayer.py
+++ package/synthSportPlayer/sportplayer.py
@@ -43,7 +43,12 @@ class player:
         """
         A function to update the current points of the player, their total points is based only on the last 10 groups of points they have gained.
         
-        [param points]: int, the points gained by the player
+        Parameters
+        ----------
+        num1 : int
+            First number to add
+        num2 : int
+            Second number to add
         """
         self.pointRec.append(points)
         if len(self.pointRec)>10:
@@ -230,52 +235,3 @@ class season:
         self.playerSum.to_csv(fldr+"/seasonPoints.csv",index=False)
         
         
-
-class liveTourn(tournament):
-    def __init__(self, playerList,pointPerRound=5):
-        super().__init__(playerList,pointPerRound)
-    
-    def playTourn(self):
-        """
-        A function to play the entire tournament. When this one is called it will only play the next step
-        """
-        if len(self.currentRound)>1:
-            nextRound = self.playRound()
-            self.currentRound = nextRound
-            self.round+=1
-            if len(self.currentRound)==1:
-                self.currentRound[0].gainPoints(self.round*self.points)
-                self.tournRes = pd.DataFrame(self.matchRec,columns=["Round", "Match",
-                                                                    "player1_id","player1_rnkPoints","player1_perform",
-                                                                    "player2_id","player2_rnkPoints","player2_perform","winner_id"])
-                return True
-            else:
-                return False
-        else:
-            return True
-        
-class liveSeason(season):
-    def __init__(self,numPlayers=16,tournToPlay=20, players=None, playerSum=None):
-        super().__init__(numPlayers=16,tournToPlay=20, players=None, playerSum=None)
-        shuffle(self.players)
-        self.currentTourn = liveTourn(self.players)
-        self.currentTournComplete = False
-        
-    def playSeason(self):
-        """
-        A function to play out the season of tournaments. when it is called it just does one step of the tournament.
-        """
-        if self.week == self.tournsToPlay:
-            return True
-        else:
-            if self.currentTournComplete:
-                shuffle(self.players)
-                self.currentTourn = liveTourn(self.players)
-
-            self.currentTournComplete = self.currentTourn.playTourn()
-            if self.currentTournComplete:
-                self.gatherPoints()
-                self.tournRecs.append(self.currentTourn.tournRes)
-                self.week+=1
-            return False
-            
